e4c9d3b66c8fec66685ab70c9f1182ff69a2382d
gio1984 <54593648+gio1984@users.noreply.github.com>
2019-10-18 14:13:04 +0300

    Add files via upload

--- cart.php
+++ cart.php
@@ -14,6 +14,7 @@
                 session_start();
                 if (isset($_SESSION['logged_in'])) {
                     $cartQuery = "SELECT ProductsName, Quantity, Price FROM t8qugi00.Products join Cart on Products.ProductId = Cart.ProductId join Customers on Cart.CustomerId = Customers.CustomerId where UserName ='" . $_SESSION['username'] . "';";
+                    //echo '<span>' . $cartQuery . '</span>';
                     if ($resultCart = mysqli_query($connection, $cartQuery)) {
                         echo '<tr class="tableCart">';
                         echo '<th class="tableCart">Product</th>';
@@ -27,6 +28,10 @@
                             echo "<td class='tableCart'>" . ($obj->Quantity * $obj->Price) . "</td>";
                             echo "</tr>";
                         }
+                        echo"<br>";
+                        echo '<form method="post" action="confirm.php">';
+                        echo '<input class="button" type="submit" value="Confirm order">';
+                        echo '</form>';
                     } else {
                         echo "<span>I didn't find anything now :( </span>";
                     }
